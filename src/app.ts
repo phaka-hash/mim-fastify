27b9edd50c0d29c05008a583e3959c67c28e50e9
phaka-hash <phaka@dosetech.co>
2025-09-09 17:22:11 +0700

    1

--- src/app.ts
+++ src/app.ts
@@ -23,7 +23,7 @@ app.register(AutoLoad, {
   options: { prefix: "/api" },
 });
 
-const port = Number(process.env.PORT || 3002);
+const port = Number(process.env.PORT || 3000);
 const host = "0.0.0.0";
 
 app.listen({ port, host }).catch((err) => {
