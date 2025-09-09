93c19a20ea0a6cb54d88e26933d7ea5e7be33144
phaka-hash <phaka@dosetech.co>
2025-09-09 17:34:33 +0700

    1

--- src/routes/mim.ts
+++ src/routes/mim.ts
@@ -5,7 +5,10 @@ import { publishToQueue } from "../mq";
 export default async function MimRoutes(fastify: FastifyInstance) {
   fastify.post<{ Body: Transaction }>("/transactions", async (req, reply) => {
     const body = req.body;
-
+    const pad = (n: number, w = 3) => n.toString().padStart(w, "0");
+    body.Invoice_no = `T0909_${pad(
+      Math.floor(Math.random() * 1000)
+    )}_${Math.floor(Math.random() * 1000)}`;
     await publishToQueue("MIMDosetech", body);
 
     return reply.code(202).send({
